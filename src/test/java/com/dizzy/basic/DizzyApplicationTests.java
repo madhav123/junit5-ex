9d637ccc216b355c34479b9452a0d73ec651c625
madhav123 <gmrdizzy2@gmail.com>
2023-02-16 05:52:37 +0530

    initial-commit'

---
+++ src/test/java/com/dizzy/basic/DizzyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dizzy.basic;
+
+import org.junit.jupiter.api.Test;
+
+class DizzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
---
+++ src/test/java/com/dizzy/mockito/integration/AccountServiceTest.java
@@ -0,0 +1,26 @@
+package com.dizzy.mockito.integration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.dizzy.model.Accounts;
+import com.dizzy.repository.AccountsRepository;
+import com.dizzy.service.AccountService;
+
+@SpringBootTest
+public class AccountServiceTest {
+	
+	@Autowired
+	AccountService accountService;
+    
+	@Test
+	public void test1() {
+		Accounts account =accountService.getAccountDetails(1);
+		System.out.println("account" + account.getBranchAddress());
+		assertNotNull(account);
+	}
+}
